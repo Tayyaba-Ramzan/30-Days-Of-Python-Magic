48e065f341a0b72369be8a7fc4bcf49104159d67
Tayyaba Ramzan <160269220+Tayyaba-Ramzan@users.noreply.github.com>
2025-02-17 22:10:03 -0800

    Day 1 Challenge Successfully Completed

---
+++ Day 1/q1.py
@@ -0,0 +1,9 @@
+# 30-Day Python Coding Challenge: From Basic to Advanced
+
+## Day 1: Hello, World!
+# Question: Write a Python program that prints "Hello, World!" to the console.
+
+# Solution:
+print("Hello, World!")
+
+#                            XXXXXXXXXXXXXXXXXXX
